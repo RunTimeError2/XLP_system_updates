5f06ddee2797fcbc5b4b7f693d301404aef79930
RunTimeError2 <songjiahao980530@126.com>
2019-03-31 09:22:39 +0800

    Added rsync conf

--- setup.sh
+++ setup.sh
@@ -9,12 +9,6 @@ which docker
 if [ $? -ne 0 ]; then
 	echo 'Docker not installed yet. Installing docker...'
 	apt-get install docker.io -y
-	if [$? -eq 0]; then
-		echo 'Docker successfully installed.'
-	else
-		echo 'An error occurred when installing docker, please check your system.'
-		exit
-	fi
 else
 	echo 'Docker already installed.'
 fi
@@ -23,12 +17,6 @@ which pip
 if [ $? -ne 0 ]; then
 	echo 'pip not installed yet. Installing pip...'
 	apt-get install python-pip -y
-	if [$? -eq 0]; then
-		echo 'Pip successfully installed.'
-	else
-		echo 'An error occurred when installing pip, please check your system.'
-		exit
-	fi
 else
 	echo 'pip already installed.'
 fi
@@ -37,12 +25,6 @@ which docker-compose
 if [ $? -ne 0 ]; then
 	echo 'Docker-compose not installed yet. Installing docker-compose...'
 	pip install docker-compose
-	if [$? -eq 0]; then
-		echo 'Docker-compose successfully installed.'
-	else
-		echo 'An error occurred when installing docker-compose, please check your system.'
-		exit
-	fi
 else
 	echo 'Docker-compose already installed.'
 fi
@@ -51,12 +33,6 @@ which nginx
 if [$? -ne 0]; then
 	echo 'Nginx not installed yet. Installing nginx...'
 	apt-get install nginx
-	if [$? -eq 0]; then
-		echo 'Nginx successfully installed.'
-	else
-		echo 'An error occurred when installing nginx, please check your system.'
-		exit
-	fi
 else
 	echo 'Nginx already installed.'
 fi
